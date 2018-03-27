524a02ec6d03bacd47aeb94a6d9b9a31de596122
blonski <225959@student.pwr.edu.pl>
2018-03-27 17:40:50 +0200

    delete Methods

--- sdizo/sdizo/Array.h
+++ sdizo/sdizo/Array.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstdlib>
 #include <stdio.h>
+#include <iostream>
 #include <string.h>
 class Array
 {
@@ -9,12 +10,29 @@ private:
 	int maxSize = 0;
 	int* arrayPointer;
 public:
+	//Contructors
 	Array();
 	Array(int size);
-	void addInt(int x); //Adds new int at the end of the array
-	void printAll();
-	void extendArray();
+	//Add fuctions
+	void addInt(int x); 
 	void addIntAtStart(int x);
+	void addOnIndex(int x, int index);
+
+
+	void printAll();
+	
+	//Searching
+	bool contains(int x);
+	void GetIndexes(int x);
+	int GetInt(int index);
+	
+	//Delete form array
+	void delLast();
+	void delFirst();
+	void delOnIndex(int index);
+
+
+	void extendArray();
 	~Array();
 };
 
--- sdizo/sdizo/ArrayTester.cpp
+++ sdizo/sdizo/ArrayTester.cpp
@@ -4,11 +4,24 @@ int main() {
 	Array *testArray=new Array();
 	Array *sizeTestArray=new Array(5);
 	testArray->addInt(21);
-	testArray->addInt(37);
-	testArray->printAll();
-	testArray->addIntAtStart(5);
+	testArray->addInt(32);
+	testArray->addInt(32);
+	testArray->addInt(32);
+	testArray->addInt(32);
+	testArray->addInt(32);
+	testArray->addInt(12);
+	testArray->addInt(55);
+	testArray->addInt(32);
 	testArray->printAll();
 	
+	testArray->delFirst();
+	testArray->printAll();
+	testArray->delOnIndex(3);
+	testArray->printAll();
+	testArray->delLast();
+	testArray->printAll();
+
 	
+
 	system("PAUSE");
 }
