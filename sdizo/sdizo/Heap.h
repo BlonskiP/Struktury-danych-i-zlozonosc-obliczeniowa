dd5a712df11a907d8a13e4121252a6bb82635f5c
blonski <225959@student.pwr.edu.pl>
2018-03-31 19:30:46 +0200

    Almost complte Heap
    pop, push and some fancy functions like getPartent

--- sdizo/sdizo/Heap.h
+++ sdizo/sdizo/Heap.h
@@ -1,12 +1,31 @@
 #pragma once
+#include <iostream>
+#include <string>
 class Heap
-{
+{//Fast Reminder
+//  Root is on top and is the biggest int (1st index in array)
+//  Left child of 'i' node is 2*i
+// Right child of 'i' node is 2*i+1
+
+
 private:
+	std::string cr, cl, cp;
 	int *array;
 	int maxSize;
 	int size;
 public:
-	Heap();
+	Heap(int startingSize);
 	~Heap();
+	void heapSort(int index);
+	void extendHeap();
+	void push(int x);
+	int pop();
+	void kill();
+	void print(std::string sp, std::string sn, int index);
+	int getParent(int index);
+	int getLeftChild(int index);
+	int getRightChild(int index);
+	void print();
+	bool contains(int value);
 };
 
