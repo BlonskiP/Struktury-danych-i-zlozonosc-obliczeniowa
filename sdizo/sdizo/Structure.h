7d34b510c15d7ba0ac6cf5471acf85ff49971dbc
blonski <225959@student.pwr.edu.pl>
2018-04-02 17:11:51 +0200

    Structure Abstract class

---
+++ sdizo/sdizo/Structure.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+
+class Structure
+{
+public:
+	//ADD METHODS
+	virtual void addElement(int x) = 0;
+	virtual void addElementOnIndex(int x, int index) = 0;
+	virtual void addElementOnBeginning(int x) = 0;
+	//DEL METHODS
+	virtual void deleteLastElement() = 0;
+	virtual void deleteIndex(int index) = 0;
+	virtual	void deleteFirst() = 0;
+	virtual	void printAll() = 0;
+	virtual int getVaule(int index) = 0;
+	virtual	void clearAll() = 0;
+	virtual	bool contains(int x) = 0;
+
+};
+
