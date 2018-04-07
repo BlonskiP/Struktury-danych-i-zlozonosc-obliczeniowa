5509ac261e85d65f36f4ce7fc96c77bfb62456ef
blonski <225959@student.pwr.edu.pl>
2018-04-07 14:49:15 +0200

    RBTree.h filled with functions

---
+++ sdizo/sdizo/RBTree.cpp
@@ -0,0 +1,54 @@
+#include "RBTree.h"
+
+
+
+RBTree::RBTree()
+{
+}
+
+
+RBTree::~RBTree()
+{
+}
+
+void RBTree::addElement(int x)
+{
+}
+
+void RBTree::addElementOnIndex(int x, int index)
+{
+}
+
+void RBTree::addElementOnBeginning(int x)
+{
+}
+
+void RBTree::deleteLastElement()
+{
+}
+
+void RBTree::deleteIndex(int index)
+{
+}
+
+void RBTree::deleteFirst()
+{
+}
+
+void RBTree::printAll()
+{
+}
+
+int RBTree::getVaule(int index)
+{
+	return 0;
+}
+
+void RBTree::clearAll()
+{
+}
+
+bool RBTree::contains(int x)
+{
+	return false;
+}
---
+++ sdizo/sdizo/RBTree.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "Structure.h"
+class RBTree : Structure
+{
+public:
+	//ADD METHODS
+	void addElement(int x);
+	void addElementOnIndex(int x, int index);
+	void addElementOnBeginning(int x);
+	//DEL METHODS
+	void deleteLastElement();
+	void deleteIndex(int index);
+	void deleteFirst();
+	void printAll();
+	int getVaule(int index);
+	void clearAll();
+	bool contains(int x);
+	RBTree();
+	~RBTree();
+private:
+};
+
